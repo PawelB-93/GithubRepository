bd1a238165c67a9e50b9d085b5deb10a2923a131
Paweł <barwinski.pawel.93@gmail.com>
2024-07-22 21:20:50 +0200

    GR-Init commit

---
+++ src/test/java/com/githubrepository/GithubRepository/GithubRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubrepository.GithubRepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
